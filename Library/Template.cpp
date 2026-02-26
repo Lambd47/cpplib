a960af4e0e886969a1b8dab9681684c8402c5e36
Lambd47 <112294802+Lambd47@users.noreply.github.com>
2026-02-25 21:20:26 -0300

    Update Template.cpp

--- Library/Template.cpp
+++ Library/Template.cpp
@@ -3,6 +3,24 @@
 #define int long long
 using namespace std;
 
+#define sz(v) ((int)(v).size())
+#define all(v) (v).begin(), (v).end()
+#define L(i, j, k) for(int i = (j); i <= (k); ++i)
+#define R(i, j, k) for(int i = (j); i >= (k); --i)
+#define pb(x) push_back(x)
+#define mp make_pair
+
+template<class T> bool chmin(T& a, const T& b){return (b<a)?a=b,true:false;}
+template<class T> bool chmax(T& a, const T& b){return (b>a)?a=b,true:false;}
+
+#define tcT template<class T
+#define tcTU tcT, class U
+#define tcTUU tcT, class ...U
+
+tcT> void re(T& x) { cin >> x; }
+tcTUU> void re(T& t, U&... u) { re(t); re(u...); }
+tcTU> void re(pair<T,U>& p) { re(p.first,p.second); }
+tcT> void re(vector<T>& x) { for(auto& a:x) re(a); }
 
 std::mt19937 rng(std::chrono::steady_clock::now().time_since_epoch().count());
 
@@ -23,4 +41,5 @@ int32_t main() {
 
 	return 0;
 }
+
 
